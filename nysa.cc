b117fc4dfd7df7702f7935c99f2e4a8d03d591ac
SzymonLukasik <szymon123lukasik@gmail.com>
2021-10-06 23:14:53 +0200

    Added initial input reading with regex validating

--- nysa.cc
+++ nysa.cc
@@ -6,21 +6,46 @@
 
 using namespace std;
 
-enum input_t{INPUT, NOT, XOR, AND, NAND, OR, NOR};
+enum input_t {
+    INPUT, NOT, XOR, AND, NAND, OR, NOR
+};
+
+static void report_error(size_t line_nr, string line) {
+    cout << "Error in line " + to_string(line_nr) + ": " + line + "\n";
+}
+
+static vector<string> tokenize_line(string line) {
+    regex whitespace_regex = regex("\\s+");
+    vector<string> container;
+    copy(sregex_token_iterator(line.begin(), line.end(), whitespace_regex, -1),
+         sregex_token_iterator(), back_inserter(container));
+    return container;
+}
+
+static vector<vector<string>> get_input() {
+    regex line_regex = regex(
+            "\\s*(NOT|XOR|AND|NAND|OR|NOR)(\\s+[1-9]\\d{0,8}){2,}\\s*"
+    );
+    string line;
+    size_t line_nr = 1;
+    vector<vector<string>> lines;
+
+    while (getline(cin, line)) {
+        if(!regex_match(line, line_regex))
+            report_error(line_nr, line);
+        else
+            lines.push_back(tokenize_line(line));
+        line_nr++;
+    }
+    return lines;
+}
 
 int main() {
-    string s;
-    regex operators_r = regex("(NOT|XOR|AND|NAND|OR|NOR)");
-    regex number_r = regex("[^[1-9]\\d{0,8}");
-    regex whitespace_r = regex("\\s+");
-
-    while (getline(cin, s)) {
-        vector<string> t;
-        copy(sregex_token_iterator(s.begin(), s.end(), whitespace_r, -1), sregex_token_iterator(),
-             back_inserter(t));
-        cout << t.size() << '\n';
-        for(size_t i = 0; i < t.size(); i++)
-            cout << t[i] << '\n';
+    vector<vector<string>> lines;
+    lines = get_input();
+    for(auto & line: lines) {
+        for (auto &s: line)
+            cout << s << " ";
         cout << "\n";
     }
 }
