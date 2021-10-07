597eeee98787f51bc24d1d38bd4f5605513d67cc
SzymonLukasik <szymon123lukasik@gmail.com>
2021-10-08 00:41:50 +0200

    Added xor and not cases to regex validation

--- nysa.cc
+++ nysa.cc
@@ -10,7 +10,7 @@ enum input_t {
     INPUT, NOT, XOR, AND, NAND, OR, NOR
 };
 
-static void report_error(size_t line_nr, string line) {
+static void report_error(size_t line_nr, const string &line) {
     cout << "Error in line " + to_string(line_nr) + ": " + line + "\n";
 }
 
@@ -23,15 +23,19 @@ static vector<string> tokenize_line(string line) {
 }
 
 static vector<vector<string>> get_input() {
-    regex line_regex = regex(
-            "\\s*(NOT|XOR|AND|NAND|OR|NOR)(\\s+[1-9]\\d{0,8}){2,}\\s*"
-    );
+    string number_pattern = "(\\s+[1-9]\\d{0,8})";
+    string not_pattern = "NOT" + number_pattern + "{1}";
+    string xor_pattern = "XOR" + number_pattern + "{2}";
+    string others_pattern = "(AND|NAND|OR|NOR)" + number_pattern + "{2,}";
+    string line_pattern = "\\s*(" + number_pattern + "|" + xor_pattern + "|" +
+                          others_pattern + ")\\s*";
+    regex line_regex = regex(line_pattern);
     string line;
     size_t line_nr = 1;
     vector<vector<string>> lines;
 
     while (getline(cin, line)) {
-        if(!regex_match(line, line_regex))
+        if (!regex_match(line, line_regex))
             report_error(line_nr, line);
         else
             lines.push_back(tokenize_line(line));
@@ -43,7 +47,7 @@ static vector<vector<string>> get_input() {
 int main() {
     vector<vector<string>> lines;
     lines = get_input();
-    for(auto & line: lines) {
+    for (auto &line: lines) {
         for (auto &s: line)
             cout << s << " ";
         cout << "\n";
